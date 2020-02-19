46d778ef502ddd0e8d29f2f7b4e22298cdf19518
Jamie Curnow <jamiec@benon.com>
2020-02-19 13:18:14 +1000

    Remove v1 importer, I doubt anyone is using v1 anymore

--- backend/index.js
+++ backend/index.js
@@ -3,47 +3,45 @@
 const logger = require('./logger').global;
 
 function appStart () {
-    const migrate             = require('./migrate');
-    const setup               = require('./setup');
-    const importer            = require('./importer');
-    const app                 = require('./app');
-    const apiValidator        = require('./lib/validator/api');
-    const internalCertificate = require('./internal/certificate');
-    const internalIpRanges    = require('./internal/ip_ranges');
+	const migrate             = require('./migrate');
+	const setup               = require('./setup');
+	const app                 = require('./app');
+	const apiValidator        = require('./lib/validator/api');
+	const internalCertificate = require('./internal/certificate');
+	const internalIpRanges    = require('./internal/ip_ranges');
 
-    return migrate.latest()
-        .then(setup)
-        .then(importer)
-        .then(() => {
-            return apiValidator.loadSchemas;
-        })
-        .then(internalIpRanges.fetch)
-        .then(() => {
+	return migrate.latest()
+		.then(setup)
+		.then(() => {
+			return apiValidator.loadSchemas;
+		})
+		.then(internalIpRanges.fetch)
+		.then(() => {
 
-            internalCertificate.initTimer();
-            internalIpRanges.initTimer();
+			internalCertificate.initTimer();
+			internalIpRanges.initTimer();
 
-            const server = app.listen(3000, () => {
-                logger.info('Backend PID ' + process.pid + ' listening on port 3000 ...');
+			const server = app.listen(3000, () => {
+				logger.info('Backend PID ' + process.pid + ' listening on port 3000 ...');
 
-                process.on('SIGTERM', () => {
-                    logger.info('PID ' + process.pid + ' received SIGTERM');
-                    server.close(() => {
-                        logger.info('Stopping.');
-                        process.exit(0);
-                    });
-                });
-            });
-        })
-        .catch(err => {
-            logger.error(err.message);
-            setTimeout(appStart, 1000);
-        });
+				process.on('SIGTERM', () => {
+					logger.info('PID ' + process.pid + ' received SIGTERM');
+					server.close(() => {
+						logger.info('Stopping.');
+						process.exit(0);
+					});
+				});
+			});
+		})
+		.catch((err) => {
+			logger.error(err.message);
+			setTimeout(appStart, 1000);
+		});
 }
 
 try {
-    appStart();
+	appStart();
 } catch (err) {
-    logger.error(err.message, err);
-    process.exit(1);
+	logger.error(err.message, err);
+	process.exit(1);
 }
