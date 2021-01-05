508bc62852bbb6aab58ff1ef083ce5ad9129a3bd
lebrou34 <abrousse@me.com>
2021-01-05 18:47:22 +0100

    Update certbot-dns-plugins.js

--- global/certbot-dns-plugins.js
+++ global/certbot-dns-plugins.js
@@ -111,12 +111,12 @@ certbot_dns_dnspod:dns_dnspod_api_token = "DNSPOD-API-TOKEN"`,
 	},
 	//####################################################//
 	gandi: {
-		display_name:      'Gandi Live DNS',
-		package_name:      'certbot_plugin_gandi',
-		package_version:   '1.2.5',
-		dependencies:      '',
-		credentials:       'certbot_plugin_gandi:dns_api_key = APIKEY',
-		full_plugin_name:  'certbot-plugin-gandi:dns',
+		display_name:     'Gandi Live DNS',
+		package_name:     'certbot_plugin_gandi',
+		package_version:  '1.2.5',
+		dependencies:     '',
+		credentials:      'certbot_plugin_gandi:dns_api_key = APIKEY',
+		full_plugin_name: 'certbot-plugin-gandi:dns',
 	},
 	//####################################################//
 	google: {
