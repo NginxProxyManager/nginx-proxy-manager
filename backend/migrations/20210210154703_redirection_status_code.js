b77f1728355a872b7340606eabc0d6b82d128a62
baruffaldi <filippo@baruffaldi.info>
2021-02-10 18:13:12 +0100

    migrations

--- backend/migrations/20210210154703_redirection_status_code.js
+++ backend/migrations/20210210154703_redirection_status_code.js
@@ -6,8 +6,8 @@ const logger	   = require('../logger').migrate;
  *
  * @see http://knexjs.org/#Schema
  *
- * @param {Object} knex
- * @param {Promise} Promise
+ * @param   {Object} knex
+ * @param   {Promise} Promise
  * @returns {Promise}
  */
 exports.up = function (knex/*, Promise*/) {
@@ -17,16 +17,16 @@ exports.up = function (knex/*, Promise*/) {
 	return knex.schema.table('redirection_host', (table) => {
 		table.integer('forward_http_code').notNull().unsigned().defaultTo(302);
 	})
-	.then(function () {
-		logger.info('[' + migrate_name + '] redirection_host Table altered');
-	});
+		.then(function () {
+			logger.info('[' + migrate_name + '] redirection_host Table altered');
+		});
 };
 
 /**
  * Undo Migrate
  *
- * @param {Object} knex
- * @param {Promise} Promise
+ * @param   {Object} knex
+ * @param   {Promise} Promise
  * @returns {Promise}
  */
 exports.down = function (knex/*, Promise*/) {
@@ -35,7 +35,7 @@ exports.down = function (knex/*, Promise*/) {
 	return knex.schema.table('redirection_host', (table) => {
 		table.dropColumn('forward_http_code');
 	})
-	.then(function () {
-		logger.info('[' + migrate_name + '] redirection_host Table altered');
-	});
+		.then(function () {
+			logger.info('[' + migrate_name + '] redirection_host Table altered');
+		});
 };
