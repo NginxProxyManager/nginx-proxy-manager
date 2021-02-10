5df9fed44aa3917106e8ebfc2e979a4d1258f289
baruffaldi <filippo@baruffaldi.info>
2021-02-10 17:16:33 +0100

    Migrations hotfix

--- backend/migrations/20210210154702_redirection_scheme.js
+++ backend/migrations/20210210154702_redirection_scheme.js
@@ -1,5 +1,5 @@
 const migrate_name = 'redirection_scheme';
-const logger       = require('../logger').migrate;
+const logger	  = require('../logger').migrate;
 
 /**
  * Migrate
@@ -15,15 +15,11 @@ exports.up = function (knex, Promise) {
 	logger.info('[' + migrate_name + '] Migrating Up...');
 
 	return knex.schema.table('redirection_host', (table) => {
-		 table.string('forward_scheme').notNull().defaultTo("$scheme");
-	 })
-	 .then(function () {
+		table.string('forward_scheme').notNull().defaultTo('$scheme');
+	})
+	.then(function () {
 		logger.info('[' + migrate_name + '] redirection_host Table altered');
-	 });
-
-	logger.info('[' + migrate_name + '] Migrating Up Complete');
-
-	return Promise.resolve(true);
+	});
 };
 
 /**
@@ -37,13 +33,9 @@ exports.down = function (knex, Promise) {
 	logger.info('[' + migrate_name + '] Migrating Down...');
 
 	return knex.schema.table('redirection_host', (table) => {
-		 table.dropColumn('forward_scheme');
-	 })
-	 .then(function () {
+		table.dropColumn('forward_scheme');
+	})
+	.then(function () {
 		logger.info('[' + migrate_name + '] redirection_host Table altered');
-	 });
-
-	logger.info('[' + migrate_name + '] Migrating Down Complete');
-
-	return Promise.resolve(true);
+	});
 };
