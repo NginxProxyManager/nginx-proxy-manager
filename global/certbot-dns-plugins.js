ae6808058cfd91c82e371374bd75050087c44f19
chaptergy <git@chaptergy.xyz>
2021-11-20 14:03:51 +0100

    Fixes dnspod credentials template

--- global/certbot-dns-plugins.js
+++ global/certbot-dns-plugins.js
@@ -186,8 +186,8 @@ dns_dnsmadeeasy_secret_key = c9b5625f-9834-4ff8-baba-4ed5f32cae55`,
 		package_name:        'certbot-dns-dnspod',
 		version_requirement: '~=0.1.0',
 		dependencies:        '',
-		credentials:         `dns_dnspod_email = "DNSPOD-API-REQUIRES-A-VALID-EMAIL"
-dns_dnspod_api_token = "DNSPOD-API-TOKEN"`,
+		credentials:         `dns_dnspod_api_id = 12345
+dns_dnspod_api_token = 1234567890abcdef1234567890abcdef`,
 		full_plugin_name: 'dns-dnspod',
 	},
 	//####################################################//
