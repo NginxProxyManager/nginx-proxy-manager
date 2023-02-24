6cdfd9ebc1ba13c708bc06a30aa042c252afb15f
Jamie Curnow <jc@jc21.com>
2023-02-24 18:32:04 +1000

    Updates chakra-ui

--- frontend/src/modals/HostCreateModal.tsx
+++ frontend/src/modals/HostCreateModal.tsx
@@ -81,7 +81,7 @@ function HostCreateModal({ isOpen, onClose }: HostCreateModalProps) {
 					{({ isSubmitting }) => (
 						<Form>
 							<ModalHeader>
-								{intl.formatMessage({ id: "certificate-authority.create" })}
+								{intl.formatMessage({ id: "host.create" })}
 							</ModalHeader>
 							<ModalCloseButton />
 							<ModalBody>
