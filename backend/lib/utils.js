7fe7e94fbd5c9a173602c0498814cb5d93195412
Kamil Skrzypinski <kamil.skrzypinski@gmail.com>
2023-02-26 20:10:25 +0100

    Mitigate CVE-2023-23596 by changing child_process.exec to child_process.execFile

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,4 +1,5 @@
 const exec = require('child_process').exec;
+const execFile = require('child_process').execFile;
 
 module.exports = {
 
@@ -9,6 +10,22 @@ module.exports = {
 	exec: function (cmd) {
 		return new Promise((resolve, reject) => {
 			exec(cmd, function (err, stdout, /*stderr*/) {
+				if (err && typeof err === 'object') {
+					reject(err);
+				} else {
+					resolve(stdout.trim());
+				}
+			});
+		});
+	},
+
+	/**
+	 * @param   {Array} cmd
+	 * @returns {Promise}
+	 */
+	execFile: function (cmd) {
+		return new Promise((resolve, reject) => {
+			execFile(cmd, function (err, stdout, /*stderr*/) {
 				if (err && typeof err === 'object') {
 					reject(err);
 				} else {
