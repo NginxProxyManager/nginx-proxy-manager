0f588baa3e8a8d56d1ce0c3e167dd9168979952b
Marcell Fülöp <marekful@domainloop.net>
2023-03-09 21:24:12 +0000

    fix: indentation

--- backend/setup.js
+++ backend/setup.js
@@ -155,30 +155,30 @@ const setupDefaultSettings = () => {
 				logger.debug('Default setting setup not required');
 			}
 		}),
-		settingModel
-			.query()
-			.select(settingModel.raw('COUNT(`id`) as `count`'))
-			.where({id: 'oidc-config'})
-			.first()
-			.then((row) => {
-				if (!row.count) {
-					settingModel
-						.query()
-						.insert({
-							id:          'oidc-config',
-							name:        'Open ID Connect',
-							description: 'Sign in to Nginx Proxy Manager with an external Identity Provider',
-							value:       'metadata',
-							meta:        {},
-						})
-						.then(() => {
-							logger.info('Added oidc-config setting');
-						});
-				}
-				if (debug_mode) {
-					logger.debug('Default setting setup not required');
-				}
-			})]);
+	settingModel
+		.query()
+		.select(settingModel.raw('COUNT(`id`) as `count`'))
+		.where({id: 'oidc-config'})
+		.first()
+		.then((row) => {
+			if (!row.count) {
+				settingModel
+					.query()
+					.insert({
+						id:          'oidc-config',
+						name:        'Open ID Connect',
+						description: 'Sign in to Nginx Proxy Manager with an external Identity Provider',
+						value:       'metadata',
+						meta:        {},
+					})
+					.then(() => {
+						logger.info('Added oidc-config setting');
+					});
+			}
+			if (debug_mode) {
+				logger.debug('Default setting setup not required');
+			}
+		})]);
 };
 
 /**
