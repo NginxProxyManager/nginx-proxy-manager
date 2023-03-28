d179887c15ac0d8a7822da7b6be36ae5055ef419
Jamie Curnow <jc@jc21.com>
2023-03-28 10:39:26 +1000

    Another fix for #2734, only chown parts of /etc/nginx

--- docker/rootfs/etc/s6-overlay/s6-rc.d/prepare/30-ownership.sh
+++ docker/rootfs/etc/s6-overlay/s6-rc.d/prepare/30-ownership.sh
@@ -11,10 +11,13 @@ chown root /tmp/nginx
 # npmuser
 chown -R npmuser:npmuser /data
 chown -R npmuser:npmuser /etc/letsencrypt
-chown -R npmuser:npmuser /etc/nginx
 chown -R npmuser:npmuser /run/nginx
 chown -R npmuser:npmuser /tmp/nginx
 chown -R npmuser:npmuser /var/cache/nginx
 chown -R npmuser:npmuser /var/lib/logrotate
 chown -R npmuser:npmuser /var/lib/nginx
 chown -R npmuser:npmuser /var/log/nginx
+
+# Don't chown entire /etc/nginx folder as this causes crashes on some systems
+chown -R npmuser:npmuser /etc/nginx/nginx.conf
+chown -R npmuser:npmuser /etc/nginx/conf.d
