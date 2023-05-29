f3c740954b3f5c3e6adbd32b3106c0abd242fdc0
Will Rouesnel <wrouesnel@wrouesnel.com>
2023-05-29 16:40:07 +1000

    Adapt CI command scripts to also support podman

--- scripts/.common.sh
+++ scripts/.common.sh
@@ -10,6 +10,17 @@ YELLOW='\E[1;33m'
 
 export BLUE CYAN GREEN RED RESET YELLOW
 
+# Identify docker-like command
+# Ensure docker exists
+if command -v docker 1>/dev/null 2>&1; then
+  export docker=docker
+elif command -v podman 1>/dev/null 2>&1; then
+  export docker=podman
+else
+  echo -e "${RED}❯ docker or podman command is not available${RESET}"
+  exit 1
+fi
+
 # Docker Compose
 COMPOSE_PROJECT_NAME="npmdev"
 COMPOSE_FILE="docker/docker-compose.dev.yml"
