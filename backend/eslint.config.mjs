18ae902a0459ac0ee25f1e25e658ba34ad0a6ca3
Zoey <zoey@z0ey.de>
2024-04-20 17:27:10 +0200

    remove global/frintend and backend folder

--- backend/eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-
-export default [{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } }, { languageOptions: { globals: globals.node } }, pluginJs.configs.recommended, eslintPluginPrettierRecommended];
