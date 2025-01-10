eea40150db547f07ed0048a8f1546000a16d2ad7
Zoey <zoey@z0ey.de>
2025-01-10 19:15:28 +0100

    fix #1371/#1373/fix hosts always regenerate on container restart/fix healtheck/change minimum PUID/PGID
    Signed-off-by: Zoey <zoey@z0ey.de>

--- rootfs/usr/local/bin/healthcheck.sh
+++ rootfs/usr/local/bin/healthcheck.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env sh
 
-export HCNPM_IP="$NPM_IPV4_BINDING"
-export HCGOA_IP="$GOA_IPV4_BINDING"
 
-
-if [ "$NPM_IPV4_BINDING" = "0.0.0.0" ]; then
-    export HCNPM_IP="127.0.0.1"
-fi
-
+export GOA="${GOA:-false}"
+export GOA_PORT="${GOA_PORT:-91}"
+export GOA_IPV4_BINDING="${GOA_IPV4_BINDING:-0.0.0.0}"
 if [ "$GOA_IPV4_BINDING" = "0.0.0.0" ]; then
-    export HCGOA_IP="127.0.0.1"
+    export GOA_IPV4_BINDING="127.0.0.1"
 fi
 
-if (if [ "$GOA" = "true" ]; then [ -f /tmp/goa/index.html ] && nc -z "$HCGOA_IP" "$GOA_PORT"; fi && if [ "$PHP82" = true ]; then cgi-fcgi -bind -connect /run/php82.sock > /dev/null 2>&1; fi && if [ "$PHP83" = true ]; then cgi-fcgi -bind -connect /run/php83.sock > /dev/null 2>&1; fi && [ "$(curl -sk https://"$HCNPM_IP":"$NPM_PORT"/api/ | jq --raw-output .status)" = "OK" ]); then
+export PHP82="${PHP82:-false}"
+export PHP83="${PHP83:-false}"
+export PHP84="${PHP84:-false}"
+
+export NPM_PORT="${NPM_PORT:-81}"
+export NPM_IPV4_BINDING="${NPM_IPV4_BINDING:-0.0.0.0}"
+if [ "$NPM_IPV4_BINDING" = "0.0.0.0" ]; then
+    export NPM_IPV4_BINDING="127.0.0.1"
+fi
+
+if (if [ "$GOA" = "true" ]; then [ -f /tmp/goa/index.html ] && nc -z "$GOA_IPV4_BINDING" "$GOA_PORT"; fi && if [ "$PHP82" = true ]; then cgi-fcgi -bind -connect /run/php82.sock > /dev/null 2>&1; fi && if [ "$PHP83" = true ]; then cgi-fcgi -bind -connect /run/php83.sock > /dev/null 2>&1; fi && if [ "$PHP84" = true ]; then cgi-fcgi -bind -connect /run/php84.sock > /dev/null 2>&1; fi && [ "$(curl -sk https://"$NPM_IPV4_BINDING":"$NPM_PORT"/api/ | jq --raw-output .status)" = "OK" ]); then
 	echo "OK"
 	exit 0
 else
--- rootfs/usr/local/bin/start.sh
+++ rootfs/usr/local/bin/start.sh
@@ -156,22 +156,22 @@ fi
 
 
 if ! echo "$PUID" | grep -q "^[0-9]\+$"; then
-    echo "PUID needs to be a number greater or equal to 1000, or equal to 0."
+    echo "PUID needs to be a number greater or equal to 99, or equal to 0."
     sleep inf
 fi
 
-if [ "$PUID" -lt "1000" ] && [ "$PUID" != "0" ]; then
-    echo "PUID needs to be a number greater or equal to 1000, or equal to 0."
+if [ "$PUID" -lt "99" ] && [ "$PUID" != "0" ]; then
+    echo "PUID needs to be a number greater or equal to 99, or equal to 0."
     sleep inf
 fi
 
 if ! echo "$PGID" | grep -q "^[0-9]\+$"; then
-    echo "PGID needs to be a number greater or equal to 1000, or equal to 0."
+    echo "PGID needs to be a number greater or equal to 99, or equal to 0."
     sleep inf
 fi
 
-if [ "$PGID" -lt "1000" ] && [ "$PGID" != "0" ]; then
-    echo "PGID needs to be a number greater or equal to 1000, or equal to 0."
+if [ "$PGID" -lt "99" ] && [ "$PGID" != "0" ]; then
+    echo "PGID needs to be a number greater or equal to 99, or equal to 0."
     sleep inf
 fi
 
@@ -377,8 +377,14 @@ if echo "$GOACLA" | grep -vq "geoip-database"; then
     if [ -s /data/etc/goaccess/geoip/GeoLite2-City.mmdb ] || [ -s /data/etc/goaccess/geoip/GeoLite2-Country.mmdb ]|| [ -s /data/etc/goaccess/geoip/GeoLite2-ASN.mmdb ]; then
         echo "All goaccess geoip databases need to be moved from etc/goaccess/geoip to goaccess/geoip inside the mounted data folder!"
     fi
-    if [ -s /data/goaccess/GeoLite2-City.mmdb ] && [ -s /data/goaccess/GeoLite2-Country.mmdb ] && [ -s /data/goaccess/GeoLite2-ASN.mmdb ]; then
-        export GOACLA="$GOACLA --geoip-database=/data/goaccess/geoip/GeoLite2-City.mmdb --geoip-database=/data/goaccess/geoip/GeoLite2-Country.mmdb --geoip-database=/data/goaccess/geoip/GeoLite2-ASN.mmdb"
+    if [ -s /data/goaccess/geoip/GeoLite2-City.mmdb ]; then
+        export GOACLA="$GOACLA --geoip-database=/data/goaccess/geoip/GeoLite2-City.mmdb"
+    fi
+    if [ -s /data/goaccess/geoip/GeoLite2-Country.mmdb ]; then
+        export GOACLA="$GOACLA --geoip-database=/data/goaccess/geoip/GeoLite2-Country.mmdb"
+    fi
+    if [ -s /data/goaccess/geoip/GeoLite2-ASN.mmdb ]; then
+        export GOACLA="$GOACLA --geoip-database=/data/goaccess/geoip/GeoLite2-ASN.mmdb"
     fi
 fi
 
@@ -467,7 +473,8 @@ if [ "$NC_AIO" = "true" ]; then
 fi
 
 
-if [ ! -s /data/npmplus/env.sha512sum ] || [ "$(cat /data/npmplus/env.sha512sum)" != "$( (grep "env\.[A-Z0-9_]\+" -roh /app/templates | sed "s|env.||g" | sort | uniq | xargs printenv; echo "1") | tr -d "\n" | sha512sum | cut -d" " -f1)" ]; then
+export TV="1"
+if [ ! -s /data/npmplus/env.sha512sum ] || [ "$(cat /data/npmplus/env.sha512sum)" != "$( (grep "env\.[A-Z0-9_]\+" -roh /app/templates | sed "s|env.||g" | sort | uniq | xargs printenv; echo "$TV") | tr -d "\n" | sha512sum | cut -d" " -f1)" ]; then
     echo "At least one env or the template version changed, all hosts will be regenerated."
     export REGENERATE_ALL="true"
 fi
