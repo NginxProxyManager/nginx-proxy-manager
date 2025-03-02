79bdc8071c572bfa15ba6a76ebe8cd2f1e482cd4
Sylphrena Kleinsasser <syl@kleinsasser.dev>
2025-03-01 19:21:42 -0500

    update comments and documentation, validate output from getent with regex

--- backend/lib/ddns_resolver/ddns_resolver.js
+++ backend/lib/ddns_resolver/ddns_resolver.js
@@ -8,13 +8,13 @@ const ddnsResolver = {
 
 	/**
      * Resolves the given address to its IP
-     * @param {String} address 
-     * @param {boolean} forceUpdate: whether to force resolution instead of using the cached value
+     * @param {String} domainName domain name of the dynamic DNS record
+     * @param {boolean} forceUpdate option to force resolution instead of using the cached value
      */
-	resolveAddress: (address, forceUpdate=false) => {
-		if (!forceUpdate && ddnsResolver._cache.has(address)) {
+	resolveAddress: (domainName, forceUpdate=false) => {
+		if (!forceUpdate && ddnsResolver._cache.has(domainName)) {
 			// Check if it is still valid
-			const value       = ddnsResolver._cache.get(address);
+			const value       = ddnsResolver._cache.get(domainName);
 			const ip          = value[0];
 			const lastUpdated = value[1];
 			const nowSeconds  = Date.now();
@@ -23,39 +23,35 @@ const ddnsResolver = {
 				return Promise.resolve(ip);
 			}
 		}
-		ddnsResolver._cache.delete(address);
+		ddnsResolver._cache.delete(domainName);
 		// Reach here only if cache value doesn't exist or needs to be updated 
-		let host = address.toLowerCase();
+		let host = domainName.toLowerCase();
 		return ddnsResolver._queryHost(host)
 			.then((resolvedIP) => {
-				ddnsResolver._cache.set(address, [resolvedIP, Date.now()]);
+				ddnsResolver._cache.set(domainName, [resolvedIP, Date.now()]);
 				return resolvedIP;
 			})
 			.catch((/*error*/) => {
 				// return input address in case of failure
-				return address;
+				logger.error(`Failed to resolve IP for ${host}`);
+				return domainName;
 			});
 	},
 
     
-	/** Private **/
-	// Properties
-	/**
-     * cache mapping host to (ip address, last updated time)
-     */
+	/** Cache mapping host to (ip address, last updated time) */
 	_cache: new Map(),
 
-	// Methods
 	/**
-     * 
-     * @param {String} host 
-     * @returns {Promise}
+     * Uses execSafe to query the IP address of the given host
+     * @param {String} host host to query
+     * @returns {Promise} resolves to the IPV4 address of the host
      */
 	_queryHost: (host) => {
 		return utils.execSafe('getent', ['ahostsv4', 'hosts', host])
 			.then((result) => {
-				if (result.length < 8) {
-					logger.error(`IP lookup for ${host} returned invalid output: ${result}`);
+				if (result.length < 8 || !/^(\d{1,3}\.){3}\d{1,3}$/.test(result)) {
+					logger.error(`IPV4 lookup for ${host} returned invalid output: ${result}`);
 					throw error.ValidationError('Invalid output from getent hosts');
 				}
 				const out = result.split(/\s+/);
