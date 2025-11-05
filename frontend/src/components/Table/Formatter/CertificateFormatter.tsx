a3a06149484ac596441a6a8e73c1f017b8b6bf4b
Jamie Curnow <jc@jc21.com>
2025-11-05 10:21:55 +1000

    Fix #4828 showing incorrect certicificate value

--- frontend/src/components/Table/Formatter/CertificateFormatter.tsx
+++ frontend/src/components/Table/Formatter/CertificateFormatter.tsx
@@ -5,5 +5,14 @@ interface Props {
 	certificate?: Certificate;
 }
 export function CertificateFormatter({ certificate }: Props) {
-	return <T id={certificate ? "lets-encrypt" : "http-only"} />;
+	let translation = "http-only";
+	if (certificate) {
+		translation = certificate.provider;
+		if (translation === "letsencrypt") {
+			translation = "lets-encrypt";
+		} else if (translation === "other") {
+			translation = "certificates.custom";
+		}
+	}
+	return <T id={translation} />;
 }
