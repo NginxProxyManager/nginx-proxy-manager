17a0887015a30c232bf59b000946a660c5f4129f
Jozef Gaal <dodog@users.noreply.github.com>
2025-11-11 16:13:56 +0100

    Add Slovak language

--- frontend/src/locale/src/HelpDoc/index.ts
+++ frontend/src/locale/src/HelpDoc/index.ts
@@ -1,8 +1,9 @@
 // import * as de from "./de/index";
 // import * as fa from "./fa/index";
 import * as en from "./en/index";
+import * as sk from "./sk/index";
 
-const items: any = { en };
+const items: any = { en, sk };
 
 const fallbackLang = "en";
 
