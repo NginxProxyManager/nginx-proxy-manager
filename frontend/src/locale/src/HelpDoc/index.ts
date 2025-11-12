b0756ca8277c50f96c218fe37f5a169eb88e3845
Jamie Curnow <jc@jc21.com>
2025-11-12 18:59:37 +1000

    Fix Russion locale, compiled file was comitted without a source file

--- frontend/src/locale/src/HelpDoc/index.ts
+++ frontend/src/locale/src/HelpDoc/index.ts
@@ -1,5 +1,5 @@
-import * as en from "./en/index";
 import * as de from "./de/index";
+import * as en from "./en/index";
 import * as ja from "./ja/index";
 
 const items: any = { en, de, ja };
@@ -7,17 +7,11 @@ const items: any = { en, de, ja };
 const fallbackLang = "en";
 
 export const getHelpFile = (lang: string, section: string): string => {
-	if (
-		typeof items[lang] !== "undefined" && 
-		typeof items[lang][section] !== "undefined"
-	) {
+	if (typeof items[lang] !== "undefined" && typeof items[lang][section] !== "undefined") {
 		return items[lang][section].default;
 	}
 	// Fallback to English
-	if (
-		typeof items[fallbackLang] !== "undefined" && 
-		typeof items[fallbackLang][section] !== "undefined"
-	) {
+	if (typeof items[fallbackLang] !== "undefined" && typeof items[fallbackLang][section] !== "undefined") {
 		return items[fallbackLang][section].default;
 	}
 	throw new Error(`Cannot load help doc for ${lang}-${section}`);
