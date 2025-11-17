c63c97930ed5179243fea801a4b455fe87541d7e
Zoey <zoey@z0ey.de>
2025-11-17 16:17:57 +0100

    remove old frontend

--- frontend-old/js/app/cache.js
+++
@@ -1,9 +0,0 @@
-const UserModel = require('../models/user');
-
-let cache = {
-    User:    new UserModel.Model(),
-    locale:  navigator.languages[0].toLowerCase()
-};
-
-module.exports = cache;
-
--- frontend-old/js/app/ui/footer/main.js
+++
@@ -1,8 +0,0 @@
-const Mn       = require('backbone.marionette');
-const template = require('./main.ejs');
-const Cache    = require('../../cache');
-
-module.exports = Mn.View.extend({
-    className: 'container',
-    template:  template
-});
