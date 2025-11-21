7010325d221eba8dafe7b1325263528b5c9a2ad6
abinas <144870133+abinas-hdb@users.noreply.github.com>
2025-11-22 00:33:27 +0900

    Update IntlProvider.tsx

--- frontend/src/locale/IntlProvider.tsx
+++ frontend/src/locale/IntlProvider.tsx
@@ -49,6 +49,7 @@ const getFlagCodeForLocale = (locale?: string) => {
 	const specialCases: Record<string, string> = {
 		ja: "jp", // Japan
 		zh: "cn", // China
+		vi: "vn", // Vietnam
 		ko: "kr", // Korea
 	};
 
