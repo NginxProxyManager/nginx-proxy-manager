910b71f61bbc6e96d6b15cb7cca63be27cee5ba9
occultsound <41059842+occultsound@users.noreply.github.com>
2025-12-15 13:24:55 +0800

    Add remaining Traditional Chinese help documentation files

---
+++ frontend/src/locale/src/HelpDoc/tw/index.ts
@@ -0,0 +1,6 @@
+export * as AccessLists from "./AccessLists.md";
+export * as Certificates from "./Certificates.md";
+export * as DeadHosts from "./DeadHosts.md";
+export * as ProxyHosts from "./ProxyHosts.md";
+export * as RedirectionHosts from "./RedirectionHosts.md";
+export * as Streams from "./Streams.md";
