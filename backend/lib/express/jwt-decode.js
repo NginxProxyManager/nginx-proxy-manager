db4b1f1cab4d5e448add11524fee02b464cbaf4d
Zoey <zoey@z0ey.de>
2026-01-19 14:58:05 +0100

    always return 401 if there is no token sent

--- backend/lib/express/jwt-decode.js
+++ backend/lib/express/jwt-decode.js
@@ -2,9 +2,19 @@ import Access from "../access.js";
 
 export default () => {
 	return async (req, res, next) => {
+		const token = req.cookies?.token;
+
+		if (!token) {
+			return res.status(401).json({
+				error: {
+					message: "Missing token",
+				},
+			});
+		}
+
 		try {
 			res.locals.access = null;
-			const access = new Access(req.cookies?.token || null);
+			const access = new Access(token);
 			await access.load();
 			res.locals.access = access;
 			next();
