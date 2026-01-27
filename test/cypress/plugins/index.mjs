1ff69516f7876e1808a5ab4bb2e5e12092ca4b6b
Jamie Curnow <jc@jc21.com>
2026-01-27 11:22:16 +1000

    Cypress ...

--- test/cypress/plugins/index.mjs
+++ test/cypress/plugins/index.mjs
@@ -1,20 +1,26 @@
-import { SwaggerValidation } from '@jc21/cypress-swagger-validation';
-import chalk from 'chalk';
+import { SwaggerValidation } from "@jc21/cypress-swagger-validation";
+import chalk from "chalk";
+import backendTask from "./backendApi/task.mjs";
 
 export default (on, config) => {
 	// Replace swaggerBase config var wildcard
-	if (typeof config.env.swaggerBase !== 'undefined') {
-		config.env.swaggerBase = config.env.swaggerBase.replace('{{baseUrl}}', config.baseUrl);
+	if (typeof config.env.swaggerBase !== "undefined") {
+		config.env.swaggerBase = config.env.swaggerBase.replace(
+			"{{baseUrl}}",
+			config.baseUrl,
+		);
 	}
 
 	// Plugin Events
-	on('task', SwaggerValidation(config));
-	on('task', require('./backendApi/task')(config));
-	on('task', {
+	on("task", SwaggerValidation(config));
+	on("task", backendTask(config));
+	on("task", {
 		log(message) {
-			console.log(`${chalk.cyan.bold('[')}${chalk.blue.bold('LOG')}${chalk.cyan.bold(']')} ${chalk.red.bold(message)}`);
+			console.log(
+				`${chalk.cyan.bold("[")}${chalk.blue.bold("LOG")}${chalk.cyan.bold("]")} ${chalk.red.bold(message)}`,
+			);
 			return null;
-		}
+		},
 	});
 
 	return config;
