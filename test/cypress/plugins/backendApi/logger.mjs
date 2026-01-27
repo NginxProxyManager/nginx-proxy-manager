7c3c59c79f33d88cf48d1346ecd9892e8eaa027a
Jamie Curnow <jc@jc21.com>
2026-01-27 11:41:12 +1000

    Fix cypress logger

--- test/cypress/plugins/backendApi/logger.mjs
+++ test/cypress/plugins/backendApi/logger.mjs
@@ -1,7 +1,7 @@
-import _ from "lodash";
-
-export default () => {
-	const arr = _.values(arguments);
-	arr.unshift('[Backend API]');
-	console.log.apply(null, arr);
+const log = (...args) => {
+	const arr = args;
+	arr.unshift("[Backend API]");
+	console.log(...arr);
 };
+
+export default log;
