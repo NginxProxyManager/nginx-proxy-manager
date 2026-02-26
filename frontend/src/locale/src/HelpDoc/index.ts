2668bdd353389f78a177770ff5871aedc684e723
晨音 <occultsound@MacBook-Air-M2.local>
2026-02-26 11:15:32 +0800

    Add Traditional Chinese locale

--- frontend/src/locale/src/HelpDoc/index.ts
+++ frontend/src/locale/src/HelpDoc/index.ts
@@ -19,21 +19,22 @@ import * as vi from "./vi/index";
 import * as zh from "./zh/index";
 import * as tr from "./tr/index";
 import * as hu from "./hu/index";
+import * as tw from "./tw/index";
 
-const items: any = { en, de, pt, es, et, ja, sk, cs, zh, pl, ru, it, vi, nl, bg, ko, ga, id, fr, tr, hu };
+const items: any = { en, de, pt, es, et, ja, sk, cs, zh, pl, ru, it, vi, nl, bg, ko, ga, id, fr, tr, hu, tw };
 
 
 const fallbackLang = "en";
 
 export const getHelpFile = (lang: string, section: string): string => {
-  if (typeof items[lang] !== "undefined" && typeof items[lang][section] !== "undefined") {
-    return items[lang][section].default;
-  }
-  // Fallback to English
-  if (typeof items[fallbackLang] !== "undefined" && typeof items[fallbackLang][section] !== "undefined") {
-    return items[fallbackLang][section].default;
-  }
-  throw new Error(`Cannot load help doc for ${lang}-${section}`);
+	if (typeof items[lang] !== "undefined" && typeof items[lang][section] !== "undefined") {
+		return items[lang][section].default;
+	}
+	// Fallback to English
+	if (typeof items[fallbackLang] !== "undefined" && typeof items[fallbackLang][section] !== "undefined") {
+		return items[fallbackLang][section].default;
+	}
+	throw new Error(`Cannot load help doc for ${lang}-${section}`);
 };
 
 export default items;
