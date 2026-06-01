35c6ac84834204c2bd86f0b72ed5bebb5e7f1288
GenticFlow Labs <genticflowlabs@gmail.com>
2026-06-01 17:04:42 +0100

    Revert vite config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 import "vitest/config";
 import { execFile } from "node:child_process";
 
@@ -44,10 +45,8 @@ export default defineConfig({
 			// e.g. use TypeScript check
 			typescript: true,
 		}),
+		tsconfigPaths(),
 	],
-	resolve: {
-		tsconfigPaths: true,
-	},
 	server: {
 		host: true,
 		port: 5173,
