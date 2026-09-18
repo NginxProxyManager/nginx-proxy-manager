0e152e8afe5f6584c43002eb99c7fb23737453f9
Amir Zalaghi <zalaghi@icloud.com>
2026-09-18 22:12:13 +0200

    Add Persian locale

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "src/App.tsx";
+import { getLocale, isRTLLocale } from "src/locale";
 
-import "@tabler/core/dist/css/tabler.min.css";
 import "@tabler/core/dist/js/tabler.min.js";
 import "./App.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+const renderApp = () => {
+	ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
+};
+
+const tablerStyles = isRTLLocale(getLocale())
+	? import("@tabler/core/dist/css/tabler.rtl.min.css")
+	: import("@tabler/core/dist/css/tabler.min.css");
+
+void tablerStyles.then(renderApp);
